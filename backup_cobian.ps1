b403feb70b6a68e1574eaf50d50fb6da2160c3cd
Meest <113111333+meest92@users.noreply.github.com>
2022-10-04 22:27:18 +0200

    Add files via upload

---
+++ backup_cobian.ps1
@@ -0,0 +1,12 @@
+﻿$date = Get-Date -Format "yyyy-MM-dd"
+$file = "C:\Program Files (x86)\Cobian Backup 11\Logs\log $date.txt"
+$logFileExists = Get-EventLog -list | Where-Object {$_.logdisplayname -eq "Backup Cobian"}
+
+if (! $logFileExists) {
+    New-EventLog -LogName "Backup Cobian" -Source "Backup Cobian"
+    }
+if(Select-String -Path $file -Pattern "ERR" -CaseSensitive){
+	Write-EventLog -log "Backup Cobian" -source "Backup Cobian" -EntryType Error -eventID 1 -Message "Se ha producido un error en la copia de seguridad de Cobian. Revise los logs."
+        }else {
+        	Write-EventLog -log "Backup Cobian" -source "Backup Cobian" -EntryType Information -eventID 0 -Message "Backup de Cobian realizado sin errores"
+        }    
